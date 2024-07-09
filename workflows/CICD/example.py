000bafa13dc7ba8902cdd983fdf60a435aff2435
Rohit Lovanshi <64128208+rohit940@users.noreply.github.com>
2024-07-09 16:44:12 +0530

    Create example.py

---
+++ workflows/CICD/example.py
@@ -0,0 +1,36 @@
+
+import typing
+from flytekit import task, workflow
+
+@task()
+def say_hello(name: str) -> str:
+    """A simple Flyte task to say "Hello".
+
+    The @task decorator allows Flyte to use this function as a Flyte task,
+    which is executed as an isolated, containerized unit of compute.
+    """
+    return f"Hello, {name}!"
+
+
+@task()
+def greeting_length(greeting: str) -> int:
+    """A task the counts the length of a greeting."""
+    return len(greeting)
+
+
+@workflow
+def wf(name: str = "world") -> typing.Tuple[str, int]:
+    """Declare workflow called `wf`.
+
+    """
+    greeting = say_hello(name=name)
+    greeting_len = greeting_length(greeting=greeting)
+    return greeting, greeting_len
+
+
+if __name__ == "__main__":
+    # Execute the workflow by invoking it like a function and passing in
+    # the necessary parameters
+    print(f"Running wf() {wf(name='passengers')}")
+~                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+~                                                             
