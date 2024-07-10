305e297a8e1f4e9ee402ffd147f29de6a153eb7d
Rohit Lovanshi <64128208+rohit940@users.noreply.github.com>
2024-07-10 12:27:46 +0530

    Update video_process_with_monitoring.py

--- workflows/video_process/video_process_with_monitoring.py
+++ workflows/video_process/video_process_with_monitoring.py
@@ -115,5 +115,5 @@ fixed_rate_lp = LaunchPlan.get_or_create(
     name="video_process_lp",
     workflow=monitoring_workflow_new_05,
     schedule=FixedRate(duration=timedelta(minutes=1)),
-    # fixed_inputs={"config_file": "workflows/video_process/config.yaml"}
+    fixed_inputs={"config_file": "workflows/video_process/config.yaml"}
 )
